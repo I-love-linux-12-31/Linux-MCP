bda5fd8853d628b7b01f0163729daad77017f0ec
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2025-11-22 18:09:21 +0300

    Added plugin for clipboard interaction

---
+++ src/plugins/clipboard/clipboard.py
@@ -0,0 +1,83 @@
+import io
+import copykitten
+from PIL import Image
+import mss
+
+import pyautogui
+
+from mcp.types import ImageContent
+from mcp.server.fastmcp import Image as MCPImage
+
+from src.modules.init_system import get_mcp
+
+mcp = get_mcp()
+
+@mcp.tool(name="Clipboard-Get-Text")
+async def get_text() -> str:
+    """
+    Returns text from clipboard.
+    """
+    return copykitten.paste()
+
+
+@mcp.tool(name="Clipboard-Set-Text")
+async def set_text(text: str) -> None:
+    """
+    Writes text to clipboard.
+    """
+    return copykitten.copy(text)
+
+@mcp.tool(name="Clipboard-Get-Image")
+async def get_image() -> ImageContent:
+    """
+    Returns image from clipboard.
+    """
+    pixels, width, height = copykitten.paste_image()
+    image = Image.frombytes(mode="RGBA", size=(width, height), data=pixels)
+
+    buffer = io.BytesIO()
+    image.save(buffer, format="PNG")
+    img_bytes = buffer.getvalue()
+    img_obj = MCPImage(data=img_bytes, format="png")
+    return img_obj.to_image_content()
+
+
+@mcp.tool(name="Clipboard-Copy-Image")
+async def copy_image(x: int, y: int, width: int, height: int) -> None:
+    """
+    Copies screen area to clipboard.
+    """
+    with mss.mss() as sct:
+        area = {"top": y, "left": x, "width": width, "height": height}
+        screenshot = sct.grab(area)
+        img = Image.new("RGBA", screenshot.size)
+        pixels = zip(
+            screenshot.raw[2::4],
+            screenshot.raw[1::4],
+            screenshot.raw[::4],
+            [255] * screenshot.width * screenshot.height
+        )
+        img.putdata(list(pixels))
+
+    copykitten.copy_image(img.tobytes("raw", "RGBA"), img.width, img.height)
+
+
+@mcp.tool(name="Clipboard-Paste-CtrlV")
+async def paste(x: int, y: int, click: bool = True) -> None:
+    """
+    Pastes clipboard using Ctrl + V.
+    x and y are coordinates. click is boolean flag, that requests click before paste.
+    """
+    if click:
+        pyautogui.click(x, y)
+    pyautogui.hotkey("ctrl", "v")
+
+@mcp.tool(name="Clipboard-Paste-CtrlShiftV")
+async def paste_alt(x: int, y: int, click: bool = True) -> None:
+    """
+    Pastes clipboard using Ctrl + Shift + V. Useful for most terminal apps.
+    x and y are coordinates. click is boolean flag, that requests click before paste.
+    """
+    if click:
+        pyautogui.click(x, y)
+    pyautogui.hotkey("ctrl", "shift", "v")
---
+++ src/plugins/clipboard/tools.py
@@ -0,0 +1,8 @@
+from .clipboard import (
+    get_text,
+    set_text,
+    get_image,
+    copy_image,
+    paste,
+    paste_alt,
+)
