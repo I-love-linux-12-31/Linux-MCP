da48fd5fe818d147d003af372ca8cc455ccbef46
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2025-12-02 06:41:00 +0300

    Legacy tools structure

---
+++ src/modules/all_mcp_functions.py
@@ -0,0 +1,48 @@
+# Clipboard
+# from .clipboard import (
+#     get_text,
+#     set_text,
+#     get_image,
+#     copy_image,
+#     paste,
+#     paste_alt,
+# )
+
+# from .cursor import (
+#     move,
+#     click,
+#     drag_and_drop,
+#     get_position,
+# )
+#
+# from .keyboard import (
+#     single_keypress,
+#     hotkey_press,
+# )
+
+from .misc import (
+    wait,
+)
+
+# from .applications import (
+#     get_applications_list
+# )
+
+# from ._windows_interaction  import (
+#     move_window,
+#     resize_window,
+#     focus_window,
+#     close_window,
+#     toggle_fullscreen_window
+# )
+#
+# from ._desktop_state import (
+#     get_desktop_state,
+#     get_window_image,
+#     get_full_desktop
+# )
+
+# from ._windows_interaction  import (
+#     move_window,
+#     resize_window,
+# )
---
+++ src/modules/misc.py
@@ -0,0 +1,34 @@
+import os
+import pyautogui
+import subprocess
+from typing import Literal
+from mcp.types import TextContent, CallToolResult
+
+from src.modules.init_system import get_mcp
+
+mcp = get_mcp()
+
+
+@mcp.tool(name="Wait")
+def wait(seconds: float = 1.5) -> None:
+    """
+    Pause execution for specified duration in seconds and do nothing. Default = 1.5.
+    Useful for waiting for applications start or other long action complete.
+    """
+    pyautogui.sleep(seconds)
+
+
+@mcp.tool(name="Call-To-Open-URI")
+def call_to_open_uri(uri: str) -> dict[str, str | int]:
+    """
+    Calls to open uri(file, directory, web-site...) with associated application.
+    Default is file/directory.
+    To call opening of other uri(website, ssh or ftp connection) you need specify protocol like:
+    uri='http://example.com/1/2/3', uri='file:///home/user/file.txt', uri='ssh://192.168.0.123', mailto://user@example.com, etc.
+    """
+    result = subprocess.run(["xdg-open", uri], capture_output=True, text=True)
+
+    response = {"statuscode": result.returncode, "comment": result.stdout, "error_message": result.stderr}
+    if result.returncode != 0:
+        raise RuntimeError(response)
+    return response
